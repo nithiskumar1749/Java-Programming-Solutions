7e7bad00967f8a1cdea16af10bf911bb547a9f8d
Nithiskumar J <nithis1749@gmail.com>
2026-08-09 11:30:16 +0530

    hashing

---
+++ Hashing/TwoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        int needed=0;
+
+        for(int i=0;i<nums.length;i++){
+            needed=target-nums[i];
+
+            if(map.containsKey(needed)){
+                return new int[]{map.get(needed),i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return null;
+    }
+}
